43d7a1469084a90bda502b80c18b171c8ada4c1f
adelegue <adelegue@hotmail.com>
2016-07-23 18:11:27 +0200

    config

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+activator stage ./target/universal/stage/bin/mypictures-api -Dlogback.configurationFile=/Users/adelegue/idea/mypictures-api/logback.xml -Dconfig.file=/Users/adelegue/idea/mypictures-api/application.prod.conf
